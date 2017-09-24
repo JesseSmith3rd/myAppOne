918d05e252cef0d1929ac1a4aae002546ab5603f
Jesse Smith <JesseSmith3rd@outlook.com>
2017-09-23 22:10:19 -0400

    add webpack.config.dev.js

---
+++ webpack.config.dev.js
@@ -0,0 +1,24 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'eventsource-polyfill', //necessary for hot re-loading with IE
+    'webpack-hot-middleware/client?reload-true',
+    './src/index'
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + 'dist', //physical files are only output to production
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './src'
+  },
+  plugins: [
+    new webpack.HotModule
+  ]
+}
