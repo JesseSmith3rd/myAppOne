fbf3859f3762bab425eb89135ff3fde540423bc3
Jesse Smith <JesseSmith3rd@outlook.com>
2017-09-25 18:11:29 -0400

    modified index.js, package-lock.json, yarn.lock, and eslint.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,28 +3,29 @@ import path from 'path';
 
 export default {
   debug: true,
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'inline-source-map',
+  noInfo: false,
   entry: [
-    'eventsource-polyfill', //necessary for hot re-loading with IE
-    'webpack-hot-middleware/client?reload-true',
-    './src/index'
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    path.resolve(__dirname, 'src/index')
   ],
   target: 'web',
   output: {
-    path: __dirname + 'dist', //physical files are only output to production
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './src'
+    contentBase: path.resolve(__dirname, 'src')
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugins()
+    new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
-      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
       {test: /(\.css)$/, loaders: ['style', 'css']},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
